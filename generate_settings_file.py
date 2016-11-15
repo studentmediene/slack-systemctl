1cf64ca1ab1a57b626295ad6963b764ee7c41d84
Thorben Dahl <thorben@sjostrom.no>
2016-11-15 21:48:09 +0100

    Add tab completion to the generate_settings_file.py dialogs

--- generate_settings_file.py
+++ generate_settings_file.py
@@ -1,6 +1,8 @@
 import jinja2
 import argparse
 import os.path
+import readline
+import subprocess
 
 
 parser = argparse.ArgumentParser(
@@ -20,6 +22,30 @@ parser.add_argument(
     help="Directory to store logs in."
 )
 
+def create_completer_func(choices):
+    def completer_func(text, state):
+        results = [t for t in choices if t.startswith(text)]
+        if len(results) <= state:
+            return None
+        else:
+            return results[state]
+    return completer_func
+
+def fetch_unit_list():
+    output = subprocess.check_output(
+        ["systemctl", "list-units", "--plain", "--no-pager", "--no-legend", "-t", "service"]
+    ).decode("utf-8")
+    lines = output.split("\n")
+    units = [line.split()[0] for line in lines if line.split()]
+    return units
+
+def create_insert_func(text_to_insert):
+    def insert_func():
+        readline.insert_text(text_to_insert)
+        readline.redisplay()
+    return insert_func
+
+
 parsed = parser.parse_args()
 choice = parsed.application
 log_dir = parsed.log_dir
@@ -30,11 +56,15 @@ choices = dict()
 
 
 if choice == "settings.yaml":
+    units = dict()
     add_more_units = True
     while add_more_units:
         unit = dict()
         # UNIT
-        print("Which unit in systemd will you connect Slack to?")
+        print("Which unit in systemd will you connect Slack to? (Try out tab completion!)")
+        # Prepare readline
+        readline.set_completer_delims(" \t\n\"\\'`@$><=;|&{(")
+        readline.set_completer(create_completer_func(fetch_unit_list()))
         unit['unit'] = input("> ")
 
         # ALLOWED COMMANDS
@@ -42,9 +72,10 @@ if choice == "settings.yaml":
         commands = {"status": True, "start": False, "stop": False,
                     "restart": False}
         finished = False
+        readline.set_completer(create_completer_func(sorted(commands.keys())))
         while not finished:
             print("\n".join(["[" + ("X" if commands[cmd] else " ") + "] " + cmd
-                             for cmd in commands]))
+                             for cmd in sorted(commands.keys())]))
             print("Toggle which command? (Leave blank when done)")
             answer = input("> ").strip().lower()
             if answer in commands:
@@ -56,14 +87,17 @@ if choice == "settings.yaml":
         unit['allowed_commands'] = [cmd for cmd in commands if commands[cmd]]
 
         # KEYWORD
-        print("What must all messages for this Slackbot start with?")
-        print("It should be something that you don't write by accident.")
-        default_keyword = "." + unit['unit']
+        readline.set_completer(lambda text, state: None)
+        print("What must all messages for interacting with this unit start with?")
+        print("It should be something that you don't start a message with by accident.")
+        default_keyword = "." + (unit['unit'].split(".")[0])
         print("Default: %s" % default_keyword)
+        readline.set_pre_input_hook(create_insert_func(default_keyword))
         keyword = input("> ") or default_keyword
+        readline.set_pre_input_hook(None)
 
         # HELP
-        print("Describe this Slackbot. End the description with a blank line.")
+        print("Describe this unit. End the description with a blank line.")
         help_lines = []
         answer = input("> ")
         while answer:
@@ -77,11 +111,12 @@ if choice == "settings.yaml":
         unit['slack_channel'] = input("> ")
 
         # Add unit
-        choices[keyword] = unit
+        units[keyword] = unit
 
         # More units?
         print("Do you want to connect Slack to more units? [yN]")
         add_more_units = True if input("> ").lower().strip() in ("y", "yes") else False
+    choices['units'] = units
 
 else:
     # SLACK
